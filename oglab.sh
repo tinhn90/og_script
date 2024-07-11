601b3a59813ba4ac5cdbd9f2d63da7bb2d240f46
tin hoang nhu <hoangnhutin@live.com>
2024-07-11 20:53:38 +0700

    update sh

---
+++ oglab.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+# Copyright 2015-2020 Parity Technologies (UK) Ltd.
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	set -e
+	if [[ $(whoami) == "root" ]]; then
+		MAKE_ME_ROOT=
+	else
+		MAKE_ME_ROOT=sudo
+	fi
+
+	if [ -f /etc/redhat-release ]; then
+		echo "Redhat Linux detected."
+		$MAKE_ME_ROOT yum update -y
+		$MAKE_ME_ROOT yum groupinstall -y "Development Tools"
+		$MAKE_ME_ROOT yum install -y cmake openssl-devel git protobuf protobuf-compiler clang clang-devel 
+	elif [ -f /etc/SuSE-release ]; then
+		echo "Suse Linux detected."
+		echo "This OS is not supported with this script at present. Sorry."
+		echo "Please refer to https://github.com/paritytech/substrate for setup information."
+		exit 1
+	elif [ -f /etc/arch-release ]; then
+		echo "Arch Linux detected."
+		$MAKE_ME_ROOT pacman -Syu --needed --noconfirm cmake gcc openssl-1.0 pkgconf git clang
+		export OPENSSL_LIB_DIR="/usr/lib/openssl-1.0";
+		export OPENSSL_INCLUDE_DIR="/usr/include/openssl-1.0"
+	elif [ -f /etc/mandrake-release ]; then
+		echo "Mandrake Linux detected."
+		echo "This OS is not supported with this script at present. Sorry."
+		echo "Please refer to https://github.com/paritytech/substrate for setup information."
+		exit 1
+	elif [ -f /etc/debian_version ]; then
+		echo "Ubuntu/Debian Linux detected."
+		$MAKE_ME_ROOT apt update
+		$MAKE_ME_ROOT apt install -y cmake pkg-config libssl-dev git gcc build-essential git protobuf protobuf-compiler clang libclang-dev
+        $MAKE_ME_ROOT apt install curl jq  gcc unzip wget lz4 -y
+	else
+		echo "Unknown Linux distribution."
+		echo "This OS is not supported with this script at present. Sorry."
+		echo "Please refer to https://github.com/paritytech/substrate for setup information."
+		exit 1
+	fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	set -e
+	echo "Mac OS (Darwin) detected."
+
+	if ! which brew >/dev/null 2>&1; then
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	fi
+
+	brew update
+	brew install openssl cmake llvm
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+	echo "FreeBSD detected."
+	echo "This OS is not supported with this script at present. Sorry."
+	echo "Please refer to https://github.com/paritytech/substrate for setup information."
+	exit 1
+else
+	echo "Unknown operating system."
+	echo "This OS is not supported with this script at present. Sorry."
+	echo "Please refer to https://github.com/paritytech/substrate for setup information."
+	exit 1
+fi
+
+cd $HOME && \
+ver="1.22.0" && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+rm "go$ver.linux-amd64.tar.gz" && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile && \
+source ~/.bash_profile && \
+go version
+
+git clone -b v0.1.0 https://github.com/0glabs/0g-chain.git
+cd 0g-chain
+make install
+0gchaind version
+
+# Customize if you need
+echo 'export MONIKER="My_Node"' >> ~/.bash_profile
+echo 'export CHAIN_ID="zgtendermint_16600-1"' >> ~/.bash_profile
+echo 'export WALLET_NAME="wallet"' >> ~/.bash_profile
+echo 'export RPC_PORT="26657"' >> ~/.bash_profile
+source $HOME/.bash_profile
+#initialize the node
+cd $HOME
+0gchaind init $MONIKER --chain-id $CHAIN_ID
+0gchaind config chain-id $CHAIN_ID
+0gchaind config node tcp://localhost:$RPC_PORT
+0gchaind config keyring-backend os
+
+# download genesis.json
+wget https://github.com/0glabs/0g-chain/releases/download/v0.1.0/genesis.json -O $HOME/.0gchain/config/genesis.json
+# Add seeds and peers to config.toml
+
+SEEDS="c4d619f6088cb0b24b4ab43a0510bf9251ab5d7f@54.241.167.190:26656,44d11d4ba92a01b520923f51632d2450984d5886@54.176.175.48:26656,f2693dd86766b5bf8fd6ab87e2e970d564d20aff@54.193.250.204:26656,f878d40c538c8c23653a5b70f615f8dccec6fb9f@54.215.187.94:26656" && \
+sed -i.bak -e "s/^seeds *=.*/seeds = \"${SEEDS}\"/" $HOME/.0gchain/config/config.toml
+
+#set minimum-gas-prices
+
+sed -i "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ua0gi\"/" $HOME/.0gchain/config/app.toml
+
+# Create service file
+
+sudo tee /etc/systemd/system/0gd.service > /dev/null <<EOF
+[Unit]
+Description=0G Node
+After=network.target
+
+[Service]
+User=$USER
+Type=simple
+ExecStart=$(which 0gchaind) start --home $HOME/.0gchain
+Restart=on-failure
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start the node
+$MAKE_ME_ROOT systemctl daemon-reload && \
+$MAKE_ME_ROOT systemctl enable 0gd && \
+$MAKE_ME_ROOT systemctl restart 0gd && \
+$MAKE_ME_ROOT journalctl -u 0gd -f -o cat
+
+#Create a wallet
+
+0gchaind keys add $WALLET_NAME
+
+#faucet
+echo "0x$(0gchaind debug addr $(0gchaind keys show $WALLET_NAME -a) | grep hex | awk '{print $3}')" > ./faucet.txt
+
+read -p "Please faucet at https://faucet.0g.ai/  and press [Enter] key to continue..."
+
+0gchaind status | jq .SyncInfo.catching_up
+
+0gchaind q bank balances $(0gchaind keys show $WALLET_NAME -a) 
+
+0gchaind tx staking create-validator \
+  --amount=1000000ua0gi \
+  --pubkey=$(0gchaind tendermint show-validator) \
+  --moniker=$MONIKER \
+  --chain-id=$CHAIN_ID \
+  --commission-rate=0.05 \
+  --commission-max-rate=0.10 \
+  --commission-max-change-rate=0.01 \
+  --min-self-delegation=1 \
+  --from=$WALLET_NAME \
+  --identity="" \
+  --website="" \
+  --details="0G to the moon!" \
+  --gas=500000 --gas-prices=99999ua0gi \
+  -y
